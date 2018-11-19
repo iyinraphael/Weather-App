d245e95ea96e6a7d1ae211504b44693c2be56cb5
iyinraphael <iyinraphael@gmail.com>
2018-11-19 13:09:16 -0800

    initial commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Iyin Raphael on 11/19/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
