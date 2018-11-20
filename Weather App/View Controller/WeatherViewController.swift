6d7b0e48e63f68126142c6a885f4143ae6a39d27
iyinraphael <iyinraphael@gmail.com>
2018-11-20 10:12:31 -0800

    location permission set up

--- Weather App/View Controller/WeatherViewController.swift
+++ Weather App/View Controller/WeatherViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        
+        //the best location Accuracy for app depends on which is suitable
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
     let weather_URL = "http://api.openweathermap.org/data/2.5/weather"
     let app_ID = "4ab8981371fde679fb6ce66dabd9f486"
+    
+    //create instance of location manager
+    let locationManager = CLLocationManager()
+    
     
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var weatherIcon: UIImageView!
